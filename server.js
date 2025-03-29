f85e1ee7085bcedd8905384caff2eaa7a310b4ae
enMasterX <bkissire1@gmail.com>
2025-03-28 22:39:03 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,32 +1,25 @@
-const express = require("express");
-const http = require("http");
-const socketIo = require("socket.io");
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = socketIo(server, {
+    cors: {
+        origin: "https://enmasterx.ftp.sh",  // Allow your domain
+        methods: ["GET", "POST"],           // Methods allowed
+        allowedHeaders: ["Content-Type"]    // Headers allowed
+    }
+});
 
-// Serve static files like HTML (if applicable)
-app.use(express.static("public"));
-
-// Log when a user connects
-io.on("connection", (socket) => {
-    console.log("A user connected");
-
-    // Log the received message and broadcast it
-    socket.on("message", (msg) => {
-        console.log("Message received:", msg); // Debugging log
-        io.emit("message", msg); // Broadcast the message to all clients
-    });
-
-    // Log when a user disconnects
-    socket.on("disconnect", () => {
-        console.log("A user disconnected");
+io.on('connection', (socket) => {
+    console.log('A user connected');
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
     });
 });
 
-// Start the server
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+// Start the server on the desired port
+server.listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
 });
