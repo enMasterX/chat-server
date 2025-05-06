51becea079fa04fc96c18922a2338d0f88fe84c7
enMasterX <bkissire1@gmail.com>
2025-05-06 17:23:41 -0500

    Updated the URL

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
     cors: {
-        origin: "https://enmasterx.ftp.sh",
+        origin: "https://unknown.ftp.sh",
         methods: ["GET", "POST"],
         allowedHeaders: ["Content-Type"]
     }
